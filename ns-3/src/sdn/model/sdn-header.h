5f3988fa2b2f38d872de373e897385bbec383fa5
Junjie Li <goals.lee@qq.com>
2017-03-15 17:27:12 +0800

    add

--- ns-3/src/sdn/model/sdn-header.h
+++ ns-3/src/sdn/model/sdn-header.h
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2016 Haoliang Chen
+ * Copyright (c) 2016 goals lee
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Haoliang Chen  <chl41993@gmail.com>
+ * Author: goals.lee  <goals.lee@qq.com>
  */
 
 #ifndef SDN_HEADER_H
