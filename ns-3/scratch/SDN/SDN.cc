b2e06d3209e22cb827b4a1fbaf3eb5fd7363080a
Junjie Li <goals.lee@qq.com>
2017-03-17 23:27:28 +0800

    add

--- ns-3/scratch/SDN/SDN.cc
+++ ns-3/scratch/SDN/SDN.cc
@@ -357,7 +357,7 @@ void VanetSim::ConfigMobility()
 
          Temp=m_nodes.Get(nodeNum+24)->GetObject<MobilityModel>();
     //Temp->SetPosition(Vector(700.0, -10.0, 0.0));//source
-      Temp->SetPosition(Vector(991.0, 2900.0, 0.0));//source
+      Temp->SetPosition(Vector(991.0, 2500.0, 0.0));//source
 
          Temp=m_nodes.Get(nodeNum+25)->GetObject<MobilityModel>();
     //Temp->SetPosition(Vector(3010.0, 2950.0, 0.0));//sink
