815623157b1a518e655c8e151e5d7adb103e0bf9
Junjie Li <goals.lee@qq.com>
2017-04-04 22:27:01 +0800

    add

--- ns-3/src/sdn/model/sdn-routing-protocol.cc
+++ ns-3/src/sdn/model/sdn-routing-protocol.cc
@@ -115,7 +115,7 @@ namespace ns3 {
 		m_packetSequenceNumber (SDN_MAX_SEQ_NUM),
 			m_messageSequenceNumber (SDN_MAX_SEQ_NUM),
 			m_helloInterval (Seconds(2)),//no change less!
-			m_rmInterval (Seconds (8)),//no change less!
+			m_rmInterval (Seconds (10)),//no change less!
 			m_ipv4 (0),
 			m_helloTimer (Timer::CANCEL_ON_DESTROY),
 			m_rmTimer (Timer::CANCEL_ON_DESTROY),
