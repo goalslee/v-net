4de9a4067fa9ace4dbed9c76ed1c58f94c6fe7d0
Junjie Li <goals.lee@qq.com>
2017-04-21 14:59:58 +0800

    fix

--- ns-3/src/sdn/model/sdn-routing-protocol.cc
+++ ns-3/src/sdn/model/sdn-routing-protocol.cc
@@ -1542,6 +1542,7 @@ namespace ns3 {
 					(it->second).m_incomeDesParm.lastIP=aodvrm.Originator;
 					Aodv_sendback(it);
 				}
+				(it->second).isDes=false;
 
 			}
 
@@ -1658,7 +1659,7 @@ namespace ns3 {
 				(it->second).m_isEstablish_positive=true;
 			else     (it->second).m_isEstablish_negative=true;  
 
-			(it->second).isDes=false;
+			
 
 		}
 
